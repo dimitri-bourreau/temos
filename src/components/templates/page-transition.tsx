96ec674b4b064790b5415e0095a0e935613083d0
Dimitri Bourreau <dimitri.bourreau@theia.fr>
2026-03-26 13:29:45 +0100

    fix: navigation was slow

--- src/components/templates/page-transition.tsx
+++ src/components/templates/page-transition.tsx
@@ -7,7 +7,7 @@ export function PageTransition({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence>
       <motion.div
         key={pathname}
         initial={{ opacity: 0, y: 8 }}
